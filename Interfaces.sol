5811ae525993a7da5bbd0aac31b6dd37c0d882e1
Drake <drakesmyth@gmail.com>
2021-11-15 15:23:09 -0600

    cruft

--- Interfaces.sol
+++ Interfaces.sol
@@ -328,26 +328,6 @@ interface TOKEN_Interface {
         returns (bool);
 
     /**
-     * @dev Destroys `amount` tokens from the caller.
-     *
-     * See {ERC20-_burn}.
-     */
-    function burn(uint256 amount) external;
-
-    /**
-     * @dev Destroys `amount` tokens from `account`, deducting from the caller's
-     * allowance.
-     *
-     * See {ERC20-_burn} and {ERC20-allowance}.
-     *
-     * Requirements:
-     *
-     * - the caller must have allowance for ``accounts``'s tokens of at least
-     * `amount`.
-     */
-    function burnFrom(address account, uint256 amount) external;
-
-    /**
      * @dev Returns `true` if `account` has been granted `role`.
      */
     function hasRole(bytes32 role, address account) external returns (bool);
